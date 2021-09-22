9c136dabec89b79902d13e115c9444c1263cfe3f
romulovieira777 <romulo.vieira777@gmail.com>
2021-09-22 12:15:54 -0300

    Alias

---
+++ Section 07 - Alias/Scripts Big Query/01 - As.sql
@@ -0,0 +1,7 @@
+-- Selecting Table Data
+SELECT
+    Customer_ID     AS ID
+  , Customer_Name   AS Name
+  , Age             AS Customer_Age
+FROM
+    DB.Customer;
---
+++ Section 07 - Alias/Scripts Postgre/01 - As.sql
@@ -0,0 +1,7 @@
+-- Selecting Table Data
+SELECT
+	customer_id 	AS "Serial Number"
+  , customer_name 	AS Name
+  , age 			AS "Customer Age"
+FROM
+	customer;
