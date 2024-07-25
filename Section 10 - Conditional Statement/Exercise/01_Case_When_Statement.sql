543f7ef5f02b6a40b67ad544669f3dd7fcce83ad
Romulo Vieira <romulo.vieira777@gmail.com>
2024-07-25 10:06:24 -0300

    Conditional Statement

---
+++ Section 10 - Conditional Statement/Exercise/01_Case_When_Statement.sql
@@ -0,0 +1,26 @@
+/*
+Quick Coding Exercise on Case When Statement
+
+Retrieve the names of customers from the `Orders` table along with a column that categorizes them as 'High Value' if
+their order amount is greater than $150, otherwise categorize them as 'Standard Value'.
+
+Table Information:
+    - The `Orders` table contains information about customer orders.
+    - Each order has a unique "OrderID" as the primary key.
+    - The "CustomerName" column stores the name of the customer.
+    - The "Product" column stores the product ordered.
+    - The "Amount" column stores the order amount.
+
+Learning objective
+Practice using CASE WHEN statements to create conditional expressions in SQL queries.
+*/
+SELECT
+    CustomerName
+  , CASE
+        WHEN Amount > 150 THEN 'High Value'
+        ELSE 'Standard Value'
+    END AS CustomerCategory
+FROM
+    Orders
+ORDER BY
+    CustomerName;
